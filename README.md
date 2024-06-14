6feb8cbd902849e93cee9a7871e93133cc3dc7e9
Eamonn McGonigle <root@cwserv.gn.gov.ie>
2024-06-14 21:56:35 +0100

    Changed to write popups to a pre-provisioned <div> rather than after a specified element (what was I thinking ?!) and to sensibly handle not being able to find that element. Styling tweaks. Added ClearSimplePopups() function

--- README.md
+++ README.md
@@ -10,6 +10,7 @@ A very simple way of generating very simple pop-up messages in pure Javascript.
 
   <body>
     <h1>This is a heading</h1>
+    <div id="simplepopup"><!-- Pop-up messages go here --></div>
     <h2>This is a sub-heading</h2>
     <p>
       [...]
@@ -17,7 +18,7 @@ A very simple way of generating very simple pop-up messages in pure Javascript.
   </body>
 
   <script>
-    SimplePopup("h2",         // This is the element to insert the pop-up after
+    SimplePopup("div#simplepopup",         // This is the element to insert the pop-up after
                 ["error",],   // A list of optional classes to apply to the popup
                 "This is an error-message",  // The (HTML) content to place into the popup
                 5000,         // Automatically remove the popup after this many milliseconds (optional, defaults to 5000)
@@ -30,6 +31,14 @@ It has a few neat features:-
 - Multiple pop-ups at the same position (i.e. inserted after the same element on the page) will stack up vertically by default
 - Respects the browser "prefer reduced motion" option to disable animations
 - Popups can be clicked on to dispose of them sooner than the scheduled automatic timeout (if any)
+- Hovering over a popup prevents it from disappearing automatically. Moving the mouse away from the popup restarts the countdown
+- If the container specified for the popups isn't found, a temporary one is created and popups are displayed in the middle(ish) of the screen
+- There is a ClearSimplePopups() function which will immediately (i.e. with no animations) purge all popups from the container
+
+```
+ClearSimplePopups("div#simplepopup")
+```
+
 
 ### Configuration
 The popups themselves have minimal styling. The idea is that you can style them by applying custom classes.  There are a few pre-defined ones:-
--- SimplePopup.js
+++ SimplePopup.js
@@ -1,4 +1,4 @@
-function SimplePopup(afterSelector, classList, content, removeAfter=5000, clearOthers=false)
+function SimplePopup(popup_stack, classList, content, removeAfter=undefined, clearOthers=false)
 {
     function remove(popup)
     {
@@ -6,31 +6,25 @@ function SimplePopup(afterSelector, classList, content, removeAfter=5000, clearO
         let animationTime = parseFloat(s)
         if (!s.endsWith("ms")) animationTime *= 1000
         const stack = popup.parentElement   // Should be div.popup-stack
-        popup.classList.remove("simple-popup-visible")
+        popup.classList.remove("visible")
         setTimeout(() => {
             popup.remove()
-            console.log(stack)
-            if (stack.childElementCount === 0)
+            if (stack && (stack.childElementCount === 0) && (stack.dataset.transient))
             {
                 stack.remove()
             }
         }, animationTime)
     }
   
-    // Look for a div#popup-stack element after the specified element. If there isn't one, create it
-    const after = document.querySelector(afterSelector)
-    const afterNextSibling = after.nextElementSibling
-    let stack
-    if ((afterNextSibling.tagName === "DIV") && (afterNextSibling.classList.contains("simple-popup-stack")))
-    {
-        stack = afterNextSibling
-    }
-    else
+    // Look for the specified container. If it doesn't exist, create a temporary one
+    let stack = document.querySelector(popup_stack)
+    if (!stack)
     {
         stack = document.createElement("div")
-        stack.classList.add("simple-popup-stack")
-        after.insertAdjacentElement("afterend", stack)
+        stack.dataset['transient'] = true
+        document.body.insertAdjacentElement("afterbegin", stack)
     }
+    stack.classList.add("simple-popup-stack")
     if (clearOthers && stack.children)
     {
         stack.querySelectorAll(".simple-popup").forEach(c => c.remove())
@@ -41,11 +35,37 @@ function SimplePopup(afterSelector, classList, content, removeAfter=5000, clearO
     p.innerHTML = content
     stack.insertAdjacentElement("beforeend", p)
     setTimeout(() => p.classList.add("visible"), 1)
-    if (removeAfter) {
+    if (removeAfter !== undefined) {
         p.dataset.timeout = setTimeout(() => remove(p), removeAfter)
     }
     p.addEventListener("click", ev => {
         p.dataset.timeout && clearTimeout(p.dataset.timeout)
         remove(p)
+    })
+    p.addEventListener("mouseover", ev => {
+        // Hovering the mouse pauses the timer
+        p.dataset.timeout && clearTimeout(p.dataset.timeout)
+        delete p.dataset.timeout
+    })
+    p.addEventListener("mouseout", ev => {
+        // Restart the timer when the mouse exits
+        if (removeAfter !== undefined) {
+            p.dataset.timeout = setTimeout(() => remove(p), removeAfter)
+        }
+    })
+}
+
+
+function ClearSimplePopups(popup_stack)
+{
+    let stack = document.querySelector(popup_stack)
+    if (!stack)
+    {
+        console.error(`${popup_stack} not found`)
+        return
+    }
+    stack.querySelectorAll("div.simple-popup").forEach(p => {
+        p.dataset.timeout && clearTimeout(p.dataset.timeout)
+        p.remove()
     })
 }
